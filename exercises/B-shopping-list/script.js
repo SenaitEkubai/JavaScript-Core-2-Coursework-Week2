1f542ca0d397b47ada4f372c3e7bfc994aea0881
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2021-01-17 00:46:07 +0000

    added a title "shopping list "

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,12 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  const title = document.createElement("h2");
+  const text = document.createTextNode("Shopping list");
+  title.appendChild(text);
+  document.getElementById("content").appendChild(title);
   const unOrderedList = document.createElement("ul");
   document.getElementById("content").appendChild(unOrderedList);
+
   for (var i = 0; i < arrayOfPeople.length; i++) {
     const listTag = document.createElement("li");
     const listText = document.createTextNode(`${arrayOfPeople[i]}`);

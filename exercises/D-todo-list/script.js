18472e5667f677deec7b02a0224462cba96592ca
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2021-01-17 14:06:37 +0000

    to do list exercise

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,30 @@
 function todoList(todos) {
   // Write your code here...
+  let unOrderedListTag = document.createElement("ul");
+  document.getElementById("content").appendChild(unOrderedListTag);
+  for (var i = 0; i < todos.length; i++) {
+    let listTag = document.createElement("li");
+    let anchorTag = document.createElement("a"); // add link element
+    anchorTag.id = "anchor"; // give the element an id
+    let listText = document.createTextNode(`${todos[i].todo}`); // text inside a list
+    anchorTag.appendChild(listText);
+    listTag.appendChild(anchorTag);
+    document
+      .getElementById("content")
+      .getElementsByTagName("ul")[0]
+      .appendChild(listTag);
+    // click function
+    var clickFunction = function strikeThrough(event) {
+      listTag.style.textDecoration = "line-through";
+    };
+    anchorTag.addEventListener("click", clickFunction);
+  }
 }
-
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);

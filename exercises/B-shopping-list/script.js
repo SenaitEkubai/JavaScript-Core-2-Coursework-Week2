3c43d5f4bd1e6597d09cd55aeb110e2aea06d4e1
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2021-01-17 00:39:54 +0000

    answered shopping list exercise

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,17 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  const unOrderedList = document.createElement("ul");
+  document.getElementById("content").appendChild(unOrderedList);
+  for (var i = 0; i < arrayOfPeople.length; i++) {
+    const listTag = document.createElement("li");
+    const listText = document.createTextNode(`${arrayOfPeople[i]}`);
+    listTag.appendChild(listText);
+    document
+      .getElementById("content")
+      .getElementsByTagName("ul")[0]
+      .appendChild(listTag);
+    listTag.style.fontSize = "28px";
+  }
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
